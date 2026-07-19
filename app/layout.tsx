a187da6a73c42d81256b342a40367d766e4e618b
ezuady89 <ezuady89@gmail.com>
2026-07-19 02:46:04 +0000

    Add TikTok Pixel tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -102,6 +102,90 @@ export default function RootLayout({
             alt=""
           />
         </noscript>
+
+        {/* TikTok Pixel */}
+        <Script id="tiktok-pixel" strategy="afterInteractive">
+          {`
+            !function(w, d, t) {
+              w.TiktokAnalyticsObject = t;
+
+              var ttq = w[t] = w[t] || [];
+
+              ttq.methods = [
+                "page",
+                "track",
+                "identify",
+                "instances",
+                "debug",
+                "on",
+                "off",
+                "once",
+                "ready",
+                "alias",
+                "group",
+                "enableCookie",
+                "disableCookie",
+                "holdConsent",
+                "revokeConsent",
+                "grantConsent"
+              ];
+
+              ttq.setAndDefer = function(target, method) {
+                target[method] = function() {
+                  target.push(
+                    [method].concat(
+                      Array.prototype.slice.call(arguments, 0)
+                    )
+                  );
+                };
+              };
+
+              for (var i = 0; i < ttq.methods.length; i++) {
+                ttq.setAndDefer(ttq, ttq.methods[i]);
+              }
+
+              ttq.instance = function(pixelId) {
+                var instance = ttq._i[pixelId] || [];
+
+                for (var i = 0; i < ttq.methods.length; i++) {
+                  ttq.setAndDefer(instance, ttq.methods[i]);
+                }
+
+                return instance;
+              };
+
+              ttq.load = function(pixelId, options) {
+                var url =
+                  "https://analytics.tiktok.com/i18n/pixel/events.js";
+
+                ttq._i = ttq._i || {};
+                ttq._i[pixelId] = [];
+                ttq._i[pixelId]._u = url;
+
+                ttq._t = ttq._t || {};
+                ttq._t[pixelId] = +new Date();
+
+                ttq._o = ttq._o || {};
+                ttq._o[pixelId] = options || {};
+
+                var script = d.createElement("script");
+                script.type = "text/javascript";
+                script.async = true;
+                script.src =
+                  url + "?sdkid=" + pixelId + "&lib=" + t;
+
+                var firstScript = d.getElementsByTagName("script")[0];
+                firstScript.parentNode.insertBefore(
+                  script,
+                  firstScript
+                );
+              };
+
+              ttq.load("D9E3JBJC77U5KEVKPKQ0");
+              ttq.page();
+            }(window, document, "ttq");
+          `}
+        </Script>
       </body>
     </html>
   );
